23b6a333647ad0c905415454b4063110d7908d25
Alaa Asfour <alaa.asfour94@outlook.com>
2024-01-11 14:06:45 -0500

    Setting up Security Configuration and Account Settings to front end

---
+++ src/main/java/com/alpine/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.alpine.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.alpine.domain.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
