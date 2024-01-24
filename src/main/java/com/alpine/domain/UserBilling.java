4e38d058c120e3197c281e6c273c2cdb556acc3e
Alaa Asfour <alaa.asfour94@outlook.com>
2024-01-23 22:23:36 -0500

    Code formatting

--- src/main/java/com/alpine/domain/UserBilling.java
+++ src/main/java/com/alpine/domain/UserBilling.java
@@ -18,22 +18,58 @@ public class UserBilling {
     @OneToOne(cascade = CascadeType.ALL)
     private UserPayment userPayment;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getUserBillingName() { return userBillingName; }
-    public void setUserBillingName(String userBillingName) { this.userBillingName = userBillingName; }
-    public String getUserBillingStreet1() { return userBillingStreet1; }
-    public void setUserBillingStreet1(String userBillingStreet1) { this.userBillingStreet1 = userBillingStreet1; }
-    public String getUserBillingStreet2() { return userBillingStreet2; }
-    public void setUserBillingStreet2(String userBillingStreet2) { this.userBillingStreet2 = userBillingStreet2; }
-    public String getUserBillingCity() { return userBillingCity; }
-    public void setUserBillingCity(String userBillingCity) { this.userBillingCity = userBillingCity; }
-    public String getUserBillingStateProvince() { return userBillingStateProvince; }
-    public void setUserBillingStateProvince(String userBillingStateProvince) { this.userBillingStateProvince = userBillingStateProvince; }
-    public String getUserBillingCountry() { return userBillingCountry; }
-    public void setUserBillingCountry(String userBillingCountry) { this.userBillingCountry = userBillingCountry; }
-    public String getUserBillingZipPostalCode() { return userBillingZipPostalCode; }
-    public void setUserBillingZipPostalCode(String userBillingZipPostalCode) { this.userBillingZipPostalCode = userBillingZipPostalCode; }
-    public UserPayment getUserPayment() { return userPayment; }
-    public void setUserPayment(UserPayment userPayment) { this.userPayment = userPayment; }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getUserBillingName() {
+        return userBillingName;
+    }
+    public void setUserBillingName(String userBillingName) {
+        this.userBillingName = userBillingName;
+    }
+    public String getUserBillingStreet1() {
+        return userBillingStreet1;
+    }
+    public void setUserBillingStreet1(String userBillingStreet1) {
+        this.userBillingStreet1 = userBillingStreet1;
+    }
+    public String getUserBillingStreet2() {
+        return userBillingStreet2;
+    }
+    public void setUserBillingStreet2(String userBillingStreet2) {
+        this.userBillingStreet2 = userBillingStreet2;
+    }
+    public String getUserBillingCity() {
+        return userBillingCity;
+    }
+    public void setUserBillingCity(String userBillingCity) {
+        this.userBillingCity = userBillingCity;
+    }
+    public String getUserBillingStateProvince() {
+        return userBillingStateProvince;
+    }
+    public void setUserBillingStateProvince(String userBillingStateProvince) {
+        this.userBillingStateProvince = userBillingStateProvince;
+    }
+    public String getUserBillingCountry() {
+        return userBillingCountry;
+    }
+    public void setUserBillingCountry(String userBillingCountry) {
+        this.userBillingCountry = userBillingCountry;
+    }
+    public String getUserBillingZipPostalCode() {
+        return userBillingZipPostalCode;
+    }
+    public void setUserBillingZipPostalCode(String userBillingZipPostalCode) {
+        this.userBillingZipPostalCode = userBillingZipPostalCode;
+    }
+    public UserPayment getUserPayment() {
+        return userPayment;
+    }
+    public void setUserPayment(UserPayment userPayment) {
+        this.userPayment = userPayment;
+    }
 }
