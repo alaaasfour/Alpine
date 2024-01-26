dd6b412114d972ec9e7cea9446ef6349f04a8c63
Alaa Asfour <alaa.asfour94@outlook.com>
2024-01-26 17:40:35 -0500

    Adding ShippingAddress Entity

--- src/main/java/com/alpine/domain/Book.java
+++ src/main/java/com/alpine/domain/Book.java
@@ -1,8 +1,10 @@
 package com.alpine.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Book {
@@ -27,6 +29,9 @@ public class Book {
     private int inStockNumber;
     @Transient
     private MultipartFile bookImage;
+    @OneToMany(mappedBy = "book")
+    @JsonIgnore
+    private List<BookToCartItem> bookToCartItemList;
 
     public Long getId() {
         return id;
@@ -156,5 +161,13 @@ public class Book {
 
     public void setBookImage(MultipartFile bookImage) {
         this.bookImage = bookImage;
+    }
+
+    public List<BookToCartItem> getBookToCartItemList() {
+        return bookToCartItemList;
+    }
+
+    public void setBookToCartItemList(List<BookToCartItem> bookToCartItemList) {
+        this.bookToCartItemList = bookToCartItemList;
     }
 }
