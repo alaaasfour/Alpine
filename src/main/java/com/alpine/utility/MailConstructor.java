a42066145f37a87f45fb5b1b1e60223553bed5ab
Alaa Asfour <alaa.asfour94@outlook.com>
2024-01-31 10:02:44 -0500

    Adding some extra book covers

--- src/main/java/com/alpine/utility/MailConstructor.java
+++ src/main/java/com/alpine/utility/MailConstructor.java
@@ -1,17 +1,26 @@
 package com.alpine.utility;
 
+import com.alpine.domain.Order;
 import com.alpine.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.Locale;
 
 @Component
 public class MailConstructor {
     @Autowired
     private Environment env;
+    @Autowired
+    private TemplateEngine templateEngine;
 
     public SimpleMailMessage constructResetTokenEmail (
             String contextPath, Locale locale, String token, User user, String password
@@ -24,5 +33,25 @@ public class MailConstructor {
         email.setText(url+message);
         email.setFrom(env.getProperty("support.email"));
         return email;
+    }
+
+    public MimeMessagePreparator constructOrderConfirmationEmail (User user, Order order, Locale locale) {
+        Context context = new Context();
+        context.setVariable("order", order);
+        context.setVariable("user", user);
+        context.setVariable("cartItemList", order.getCartItemList());
+        String text = templateEngine.process(" ", context);
+
+        MimeMessagePreparator messagePreparator = new MimeMessagePreparator() {
+            @Override
+            public void prepare(MimeMessage mimeMessage) throws Exception {
+                MimeMessageHelper email = new MimeMessageHelper(mimeMessage);
+                email.setTo(user.getEmail());
+                email.setSubject("Order Confirmation - " + order.getId());
+                email.setText(text, true);
+                email.setFrom(new InternetAddress("alaa.asfour94@outlook.com"));
+            }
+        };
+        return messagePreparator;
     }
 }
