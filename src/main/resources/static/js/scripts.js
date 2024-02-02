5ab7f4f3fb753ef5e319d8bff91cb21318e3e408
Alaa Asfour <alaa.asfour94@outlook.com>
2024-02-01 22:09:56 -0500

    Implement new password config and adding its script

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,9 +1,25 @@
-
 function checkBillingAddress() {
     if ($("#theSameAsShippingAddress").is(":checked")) {
         $(".billingAddress").prop("disabled", true);
     } else {
         $(".billingAddress").prop("disabled", false);
+    }
+}
+function checkPasswordMatch() {
+    var password = $("#txtNewPassword").val();
+    var confirmPassword = $("#textConfirmPassword").val();
+
+    if (password == "" && confirmPassword == "") {
+        $("#checkPasswordMatch").html("");
+        $("updateUserInfoButton").prop('disabled', false);
+    } else {
+        if (password != confirmPassword) {
+            $("#checkPasswordMatch").html("Passwords do not match!");
+            $("updateUserInfoButton").prop('disabled', true);
+        } else {
+            $("#checkPasswordMatch").html("Passwords match!");
+            $("updateUserInfoButton").prop('disabled', false);
+        }
     }
 }
 $(document).ready(function(){
@@ -13,4 +29,6 @@ $(document).ready(function(){
         $('#update-item-'+id).css('display', 'inline-block');
     });
     $("#theSameAsShippingAddress").on('click', checkBillingAddress);
+    $("txtConfirmPassword").keyup(checkPasswordMatch);
+    $("txtNewPassword").keyup(checkPasswordMatch);
 });
