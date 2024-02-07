bd7f247d887c051bde5008e8368f049b7ab02889
Alaa Asfour <alaa.asfour94@outlook.com>
2024-02-06 23:06:09 -0500

    Adding comments to OrderRepository class

--- src/main/java/com/alpine/repository/OrderRepository.java
+++ src/main/java/com/alpine/repository/OrderRepository.java
@@ -4,4 +4,5 @@ import com.alpine.domain.Order;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
+    // This interface inherits basic CRUD operations from CrudRepository and does not define any additional custom methods.
 }
