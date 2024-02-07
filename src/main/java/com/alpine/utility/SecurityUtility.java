1da21b429976be444bcbe773a731fc3322338f48
Alaa Asfour <alaa.asfour94@outlook.com>
2024-02-06 23:19:42 -0500

    Adding Comments to SecurityUtility class

--- src/main/java/com/alpine/utility/SecurityUtility.java
+++ src/main/java/com/alpine/utility/SecurityUtility.java
@@ -11,21 +11,26 @@ import java.util.Random;
 public class SecurityUtility {
     private static final String SALT = "salt"; // Salt should be protected carefully
 
+    // Bean for creating a BCryptPasswordEncoder
     @Bean
     public static BCryptPasswordEncoder passwordEncoder() {
+        // Using BCryptPasswordEncoder with strength 12 and a custom SecureRandom salt
         return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
     }
 
+    // Method to generate a random password
     @Bean
     public static String randomPassword() {
         String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
         StringBuilder salt = new StringBuilder();
         Random rand = new Random();
 
+        // Append random characters until the password reaches the desired length
         while (salt.length() < 18) {
             int index = (int) (rand.nextFloat()*SALTCHARS.length());
             salt.append(SALTCHARS.charAt(index));
         }
+        // Convert StringBuilder to String and return
         String saltStr = salt.toString();
         return saltStr;
     }
