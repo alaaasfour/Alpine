d4e1a353e87a64f63a4fbb42caba77e5094812f2
Alaa Asfour <alaa.asfour94@outlook.com>
2024-02-06 23:27:55 -0500

    BillingAddressService & BillingAddressServiceImpl Comments

--- src/main/java/com/alpine/service/BillingAddressService.java
+++ src/main/java/com/alpine/service/BillingAddressService.java
@@ -4,5 +4,12 @@ import com.alpine.domain.BillingAddress;
 import com.alpine.domain.UserBilling;
 
 public interface BillingAddressService {
+    /**
+     * Sets the billing address based on the user billing information.
+     *
+     * @param userBilling    The user's billing information.
+     * @param billingAddress The billing address to be set.
+     * @return The updated billing address after setting it based on the user billing information.
+     */
     BillingAddress setByUserBilling(UserBilling userBilling, BillingAddress billingAddress);
 }
--- src/main/java/com/alpine/service/impl/BillingAddressServiceImpl.java
+++ src/main/java/com/alpine/service/impl/BillingAddressServiceImpl.java
@@ -8,6 +8,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class BillingAddressServiceImpl implements BillingAddressService {
     public BillingAddress setByUserBilling(UserBilling userBilling, BillingAddress billingAddress) {
+        /**
+         * Sets the billing address based on the user billing information.
+         *
+         * @param userBilling    The user's billing information.
+         * @param billingAddress The billing address to be set.
+         * @return The updated billing address after setting it based on the user billing information.
+         */
         billingAddress.setBillingAddressName(userBilling.getUserBillingName());
         billingAddress.setBillingAddressStreet1(userBilling.getUserBillingStreet1());
         billingAddress.setBillingAddressStreet2(userBilling.getUserBillingStreet2());
