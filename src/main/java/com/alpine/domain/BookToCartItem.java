ad0353570fbd00147457877a47cc5b498abb652e
Alaa Asfour <alaa.asfour94@outlook.com>
2024-02-06 22:20:10 -0500

    BookToCartItem Class comments

--- src/main/java/com/alpine/domain/BookToCartItem.java
+++ src/main/java/com/alpine/domain/BookToCartItem.java
@@ -6,34 +6,33 @@ import javax.persistence.*;
 public class BookToCartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    // Unique identifier for the BookToCartItem entity
     private Long id;
     @ManyToOne
     @JoinColumn(name = "book_id")
+    // The associated book entity
     private Book book;
     @ManyToOne
     @JoinColumn(name = "cart_item_id")
+    // The associated cart item entity
     private CartItem cartItem;
 
+    // Getter and setter methods for each attribute
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public Book getBook() {
         return book;
     }
-
     public void setBook(Book book) {
         this.book = book;
     }
-
     public CartItem getCartItem() {
         return cartItem;
     }
-
     public void setCartItem(CartItem cartItem) {
         this.cartItem = cartItem;
     }
