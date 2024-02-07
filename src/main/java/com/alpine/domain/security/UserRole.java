712dfdbeb69ffa76c8135ae5f5ad720b8f481779
Alaa Asfour <alaa.asfour94@outlook.com>
2024-02-06 22:55:46 -0500

    Adding Comments to the UserRole class

--- src/main/java/com/alpine/domain/security/UserRole.java
+++ src/main/java/com/alpine/domain/security/UserRole.java
@@ -6,17 +6,17 @@ import javax.persistence.*;
 @Entity
 @Table(name="user_role")
 public class UserRole {
-
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
+    // The unique identifier for the user role.
     private Long userRoleId;
-
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="user_id")
+    // The User entity associated with this user role.
     private User user;
-
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="role_id")
+    // The Role entity associated with this user role.
     private Role role;
 
     public UserRole(User user, Role role) {
@@ -24,26 +24,23 @@ public class UserRole {
         this.role = role;
     }
     public UserRole() {}
+
+    // Getter and setters
     public Long getUserRoleId() {
         return userRoleId;
     }
-
     public void setUserRoleId(Long userRoleId) {
         this.userRoleId = userRoleId;
     }
-
     public User getUser() {
         return user;
     }
-
     public void setUser(User user) {
         this.user = user;
     }
-
     public Role getRole() {
         return role;
     }
-
     public void setRole(Role role) {
         this.role = role;
     }
