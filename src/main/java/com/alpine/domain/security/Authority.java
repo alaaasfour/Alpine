f6b898078e80524a8dfe3d32a278ca71bf6f9090
Alaa Asfour <alaa.asfour94@outlook.com>
2024-02-06 22:49:19 -0500

    Adding Comments to the Authority Class

--- src/main/java/com/alpine/domain/security/Authority.java
+++ src/main/java/com/alpine/domain/security/Authority.java
@@ -3,11 +3,12 @@ import org.springframework.security.core.GrantedAuthority;
 
 public class Authority implements GrantedAuthority{
     private final String authority;
-
+    // Constructor to set the authority
     public Authority(String authority) {
         this.authority = authority;
     }
 
+    // Method to retrieve the authority
     @Override
     public String getAuthority() {
         return authority;
